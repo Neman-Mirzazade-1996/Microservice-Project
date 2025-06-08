03ea4c49a557d53d870c3b53f44138320d85c7a7
nemanmirzazada <neman.mirzezade@gmail.com>
2025-06-08 13:48:07 +0400

    gradle tasks completed successfully

--- Order-ms/src/main/java/com/neman/orderms/Client/ProductClient.java
+++ Order-ms/src/main/java/com/neman/orderms/Client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient(name = "product-service", path = "/api/v1/products", url = "http://localhost:9091")
+@FeignClient(name = "product-service", path = "/api/v1/products", url = "http://localhost:5020")
 public interface ProductClient {
 
     @GetMapping("/get/{id}")
